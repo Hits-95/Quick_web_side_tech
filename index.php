3f8ba8c4ea58c1bce98ee8b7e916dbb4ce7e0e9a
Ahire Hitesh <hbahire2014@gmail.com>
2020-12-18 19:06:26 +0530

    new

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <!-- Header -->
+  <?php include_once('header.php');?>
+  <!-- Header -->
+  <?php include_once('hero.php');?>
+  <!-- gallery-->
+  <?php include_once('gallery.php'); ?>
+  <!-- pricing -->
+  <?php include_once('pricing.php');?>
+  <!-- statics -->
+  <?php include_once('statics.php');?>
+  <!-- contact -->
+  <?php include_once('contact.php');?>
+  <!-- Footer -->
+  <?php include_once('footer.php');?>
+</body>
+
+</html>
